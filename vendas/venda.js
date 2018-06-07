3d4d064ff45423e1308e94d7e782016ab9f60eec
oliverjose <djoseantonyo@gmail.com>
2018-06-07 06:40:37 -0300

    commite

--- vendas/venda.js
+++ vendas/venda.js
@@ -2,19 +2,22 @@ const env = require('../.env')
 const Telegraf = require('telegraf')
 const bot = new Telegraf(env.token)
 
-bot.start( async(ctx, next) => {
+bot.start( async ctx => {
     const from = ctx.update.message.from
     console.log(from)
     await ctx.reply(`Seja bem vindo, ${from.first_name}!`)
-    next()
 })
 
 bot.on('text', async (ctx, next) => {
-        await ctx.reply('Você gostaria de fazer um orçamento?')
-        next()
+    await ctx.reply(`${ctx.update.message.text}, Você gostaria de fazer um orçamento?`)
+    next()
 })
- bot.hears('sim', ctx => ctx.reply('Otimo.'))
- bot.hears(['não','nao'], ctx => ctx.reply('Tudo bem, em que posso te ajudar?'))
+
+bot.hears('sim', async (ctx, next) => {
+    await ctx.reply('Otimo, e o que procura?')
+    next()
+})
+bot.hears(['não','nao'], ctx => ctx.reply('Tudo bem, em que posso te ajudar?'))
 
 
 
