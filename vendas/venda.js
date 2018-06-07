c16b75020d70fbd68c7802ca2df9cf2a20acbe93
oliverjose <djoseantonyo@gmail.com>
2018-06-07 13:19:30 -0300

    Estrutura p atendimento

--- vendas/venda.js
+++ vendas/venda.js
@@ -1,24 +1,30 @@
-const env = require('../.env')
-const Telegraf = require('telegraf')
-const bot = new Telegraf(env.token)
+const env = require('../.env');
+const Telegraf = require('telegraf');
+const Markup = require('telegraf/markup');
+const moment = require('moment');
+const bot = new Telegraf(env.token);
 
-bot.start( async ctx => {
-    const from = ctx.update.message.from
-    console.log(from)
-    await ctx.reply(`Seja bem vindo, ${from.first_name}!`)
+const tecladoOrcPed = Markup.keyboard([
+    ['Orçamento', 'Pedido']
+]);
+
+bot.start(async ctx => {
+    if(ctx.update.message.from.id === 123){
+        await ctx.reply(`Seja bem vindo, ${ctx.update.message.from.first_name}!`)
+        await ctx.reply(`Para facilitar e agilizar o atendimento sempre que possivel \nirei disponibilizar um teclado`
+        +`\nComeçamos... O que você gostaria de fazer?`,
+        Markup.keyboard(['Orçamento', 'Pedido']).resize().oneTime().extra())
+    } else {
+       await ctx.reply(`Por favor digite seu CNPJ/CPF, sem digitos somente numero.`)
+       bot.hears(/(\s{11})/, ctx => {
+           const cpf = moment(ctx.match[1], 'aaaaaaaaaaa')
+           ctx.reply(`CPF: ${cpf.toString('aaaaaaaaaaa')}`)
+       })
+    } 
+});
+
+bot.hears(['Orçamento', 'Pedido'], async ctx => {
+    await ctx.reply(`Ok! Vamos começar a preparação do seu ${ctx.match}`)
 })
 
-bot.on('text', async (ctx, next) => {
-    await ctx.reply(`${ctx.update.message.text}!, Você gostaria de fazer um orçamento?`)
-    next()
-})
-
-bot.hears('sim', async ctx => {
-    await ctx.reply('Otimo, e o que procura?')
-})
-bot.hears(['não','nao'], ctx => ctx.reply('Tudo bem, em que posso te ajudar?'))
-
-
-
-
-bot.startPolling()
+bot.startPolling();
